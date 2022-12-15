5df3e9cc548513a84deb67f9b3144e2e13398ad1
Suraj Nanavare <surajrajaram20@gmail.com>
2022-12-15 18:07:28 +0530

    2

--- Leetcode/268.Missing_Number.cpp
+++ Leetcode/268.Missing_Number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) 
     {
-        int a=nums.size();
+        int a = nums.size();
         vector<int>ans(a+1,0);
         for(int i=0;i<a;i++)
         {

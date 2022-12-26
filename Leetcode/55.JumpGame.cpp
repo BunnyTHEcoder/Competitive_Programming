12de3ecbd1708fdce22a81a1c595222865e9c662
Suraj Nanavare <surajrajaram20@gmail.com>
2022-12-26 14:00:58 +0530

    10

---
+++ Leetcode/55.JumpGame.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+     bool canJump(vector<int>& nums) {
+        int reach=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(reach<i)
+            {
+                return false;
+            }
+            else
+            {
+                reach=max(reach,i+nums[i]);
+            }
+       }
+        return true;
+    }
+};

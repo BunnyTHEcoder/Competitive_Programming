efa60b7124cff826bbd49768d6d180ec1e5f14d9
Suraj Nanavare <90819413+BunnyTHEcoder@users.noreply.github.com>
2023-01-25 19:57:02 +0530

    Update 118.Pascal's_Triangle.cpp

--- Leetcode/118.Pascal's_Triangle.cpp
+++ Leetcode/118.Pascal's_Triangle.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
+    vector<vector<int>>  generate(int numRows) {
 	vector<vector<int>> ret;
 	for (int i = 0; i < numRows; i++) {
 		vector<int> row(i + 1, 1);

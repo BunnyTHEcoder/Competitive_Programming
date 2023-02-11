a7c9462b8b8c8148f0ce54148e03f36a1f3801d1
Suraj Nanavare <90819413+BunnyTHEcoder@users.noreply.github.com>
2023-02-11 20:52:43 +0530

    Update 100.Same_Tree.cpp

--- Leetcode/100.Same_Tree.cpp
+++ Leetcode/100.Same_Tree.cpp
@@ -3,7 +3,7 @@ class Solution{
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(!p && !q)    return true;
-        if(!p || !q)    return false;
+        if (!p || !q)    return false;
             return (p->val == q->val && isSameTree(p->right, q->right) && isSameTree(p->left, q->left));
     }
 };
